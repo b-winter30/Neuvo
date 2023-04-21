b955ff2a5dd832b93f9834ea9744eefeed27dbad
b-winter30 <b-winter@hotmail.co.uk>
2023-04-21 20:22:33 +0100

    Adding .out and .err to .gitignore and updating submit.sh

---
+++ submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash --login
+#
+#SBATCH --job-name=neuro_object # Job name
+#SBATCH --account=scw1978
+#SBATCH --nodes=1
+#SBATCH --mem-per-cpu=32000MB
+#SBATCH --time=3-00:00
+#SBATCH --output=neuro_object.out.%J
+#SBATCH --error=neuro_object.err.%J
+#SBATCH --mail-type=ALL
+
+module load anaconda
+source activate
+conda activate tf
+conda install -c anaconda cudnn
+python neuro.py
